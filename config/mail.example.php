5a8ee631b499a231481a562780f56b155b40e092
Samah Abozaid <samah.abozaid89@gmail.com>
2026-06-24 10:44:55 +0200

    Remove exposed SMTP credentials from example config

--- config/mail.example.php
+++ config/mail.example.php
@@ -3,9 +3,9 @@
 return [
     'host'     => 'smtp.gmail.com',
     'port'     => 587,
-    'username' => 'samah.abozaid89@gmail.com',
-    'password' => 'fano xmbh nyfr nqut',  // App Password (not your Gmail password)
-    'from'     => 'samah.abozaid89@gmail.com',
+    'username' => 'votre.adresse@gmail.com',
+    'password' => 'votre_app_password_gmail',  // App Password (not your Gmail password)
+    'from'     => 'votre.adresse@gmail.com',
     'from_name'=> 'Ferme Mitopie',
     'to'       => 'contact@mitopie.fr',         // Address receiving contact & reservation emails
 ];
